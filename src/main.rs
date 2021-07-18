085f8f9a0a50c2b83594810e0144a359179ff1a1
Joshua Toyota <jearsh@gmail.com>
2021-07-18 19:33:01 -0400

    outline bare-bone object structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+#[derive(Clone,Debug)]
+pub struct AppState {
+    cursor: Pos,
+    screen_area: Pos,
+    table: Table,
+    mode: Mode,
+}
+
+// NOTE: Mode(s) probably shouldn't be enums, since they need to contain fairly dynamic data
+#[derive(Clone,Copy,Debug)]
+enum Mode {
+    Normal,
+    VisualFile,
+    VisualRow(Pos, Pos),
+    VisualCol(Pos, Pos),
+    VisualBlk(Pos, Pos),
+}
+
+#[derive(Clone,Copy,Debug)]
+struct Pos {
+    row: usize,
+    col: usize,
+}
+
+#[derive(Clone,Debug)]
+struct Table {
+    cells: Vec<usize>,
+    columns: Vec<Column>,
+    area: Pos,
+}
+
+#[derive(Clone,Debug)]
+struct Column {
+    header: String,
+    width: usize,
+}
+
 fn main() {
     println!("Hello, world!");
 }
