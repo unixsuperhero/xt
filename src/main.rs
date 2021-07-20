e5e10b2931f719cfb801f489aea55de64cbaa9b7
Joshua Toyota <jearsh@gmail.com>
2021-07-20 17:54:28 -0400

    restructure using inline mods

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,45 @@
-#[derive(Clone,Debug)]
-pub struct AppState {
-    cursor: Pos,
-    screen_area: Pos,
-    table: Table,
-    mode: Mode,
+
+mod app {
+    use crate::database::*;
+
+    #[derive(Clone,Debug)]
+    pub struct AppState {
+        cursor: Pos,
+        screen_area: Pos,
+        table: Table,
+        mode: Mode,
+    }
+
+    // NOTE: Mode(s) probably shouldn't be enums, since they need to contain fairly dynamic data
+    #[derive(Clone,Copy,Debug)]
+    enum Mode {
+        Normal,
+        VisualFile,
+        VisualRow(Pos, Pos),
+        VisualCol(Pos, Pos),
+        VisualBlk(Pos, Pos),
+    }
 }
 
-// NOTE: Mode(s) probably shouldn't be enums, since they need to contain fairly dynamic data
-#[derive(Clone,Copy,Debug)]
-enum Mode {
-    Normal,
-    VisualFile,
-    VisualRow(Pos, Pos),
-    VisualCol(Pos, Pos),
-    VisualBlk(Pos, Pos),
-}
+mod database {
+    #[derive(Clone,Debug)]
+    pub struct Table {
+        cells: Vec<usize>,
+        columns: Vec<Column>,
+        area: Pos,
+    }
 
-#[derive(Clone,Copy,Debug)]
-struct Pos {
-    row: usize,
-    col: usize,
-}
+    #[derive(Clone,Debug)]
+    pub struct Column {
+        header: String,
+        width: usize,
+    }
 
-#[derive(Clone,Debug)]
-struct Table {
-    cells: Vec<usize>,
-    columns: Vec<Column>,
-    area: Pos,
-}
-
-#[derive(Clone,Debug)]
-struct Column {
-    header: String,
-    width: usize,
+    #[derive(Clone,Copy,Debug)]
+    pub struct Pos {
+        row: usize,
+        col: usize,
+    }
 }
 
 fn main() {
