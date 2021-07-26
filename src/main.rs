c252bf319e5172648d8efd0d9e8cf18191e88d65
Joshua Toyota <jearsh@gmail.com>
2021-07-26 17:16:55 -0400

    start using modules from main()

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,19 @@ mod app;
 mod database;
 mod csv_loader;
 
+use {
+    database::Database,
+    csv_loader::CsvLoader,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut db = Database::new();
+    println!("db before: {:?}\n", &db);
+
+    let tb = CsvLoader::from_path("test/large.csv").unwrap();
+    println!("builder: \r\t{:?}\n", tb);
+
+    db.load_table(&tb);
+    println!("db after: {:?}\n", &db);
+
 }
