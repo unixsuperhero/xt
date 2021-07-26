d0094bc6e155a5dc014d5498d9f1712219d92211
Joshua Toyota <jearsh@gmail.com>
2021-07-25 22:07:30 -0400

    got rid of incomplete mode placeholder

--- src/app.rs
+++ src/app.rs
@@ -5,17 +5,6 @@ pub struct AppState<'a> {
     cursor: Pos,
     screen_area: Pos,
     table: Table<'a>,
-    mode: Mode,
-}
-
-// NOTE: Mode(s) probably shouldn't be enums, since they need to contain fairly dynamic data
-#[derive(Clone,Copy,Debug)]
-enum Mode {
-    Normal,
-    VisualFile,
-    VisualRow(Pos, Pos),
-    VisualCol(Pos, Pos),
-    VisualBlk(Pos, Pos),
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
